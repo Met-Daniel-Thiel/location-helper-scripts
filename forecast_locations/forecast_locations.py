035624fa3daa6a103f845db7e146267ec6d667b3
daniel.thiel <daniel.thiel@metoffice.gov.uk>
2023-09-29 10:49:48 +0100

    small changes

--- forecast_locations/forecast_locations.py
+++ forecast_locations/forecast_locations.py
@@ -4,13 +4,13 @@ import json
 import os
 import webbrowser
 
-# Manual input location info from Gazetteer
+# Manual input home location info from Gazetteer
 # https://github.com/MetOffice/locations-search-pkg-os/tree/master/src/main/resources/gazetteer
 c_lon = -0.4
 c_lat = 51.67
-name = "North Watford"
+name = "North Watford (gazetter)"
 
-# Create basic map and add the gazetteer location
+# Create map and add the home location
 map_uk = folium.Map(location=[c_lat,c_lon], zoom_start=14)
 folium.Marker([c_lat,c_lon], 
               icon=folium.Icon(icon="home",color="red"),
@@ -20,7 +20,6 @@ folium.Marker([c_lat,c_lon],
 # Add all forecast locations within 0.2 lat and 0.2 lon of the gazetteer location
 with open('forecast_locations/forecast_locations.json', 'r') as f:
     forecast_locations = json.load(f)
-
 
 for i in forecast_locations:
     if i['domestic'] == True:
