3040c408cae6a610d09499b4848cc23d108536a9
Daniel Thiel <daniel.thiel@metoffice.gov.uk>
2024-10-07 08:22:11 +0100

    Updated to use geodesic distance between points

--- forecast_locations.py
+++ forecast_locations.py
@@ -3,24 +3,25 @@ import folium
 import json
 import os
 import webbrowser
-import geopy
-import math
 from folium.features import DivIcon
+from geopy.distance import geodesic 
+from geopy import Nominatim
 
 
 # Manual input home location info from Gazetteer
 # https://github.com/MetOffice/locations-search-pkg-os/tree/master/src/main/resources/gazetteer
 
 # Location name or Postcode
-location = "WD17 4TE"
+location = "Crediton"
+location_count = 10
 
 # get cords for location
-geocoder = geopy.Nominatim(user_agent="capt dan")
+geocoder = Nominatim(user_agent="capt dan")
 location_raw = geocoder.geocode(location)
 c_lat, c_long = location_raw.latitude, location_raw.longitude
 
 # Create map and add the home location
-map_uk = folium.Map(location=[c_lat,c_long], zoom_start=13)
+map_uk = folium.Map(location=[c_lat,c_long], zoom_start=12)
 folium.Marker([c_lat,c_long], icon=folium.Icon(icon="home",color="red")).add_to(map_uk)
 folium.Marker([c_lat,c_long], icon=DivIcon(icon_size=(250,30),icon_anchor=(0,0), html=f'<div style="font-size: 20pt" <div>{location}</div>',)).add_to(map_uk)
 
@@ -33,13 +34,11 @@ closest_locations = {100:""} # initilise dictionary, must have at least one key 
 for i in forecast_locations:
     if i['domestic'] == True:
         lat, long = i['position']['lat'], i['position']['lon']
-        lat_diff = max(lat, c_lat) - min(lat, c_lat)
-        long_diff = max(long, c_long) - min(long, c_long)
-        distance = math.sqrt(lat_diff ** 2 + long_diff ** 2)
+        distance = geodesic((c_lat, c_long), (lat, long)).km
         
         if distance < max(list(closest_locations.keys())):
             closest_locations[distance] = {"name":i['name'], "lat":lat, "long":long}
-            if len(closest_locations) > 10:
+            if len(closest_locations) > location_count:
                 closest_locations.pop(max(list(closest_locations.keys())))
         
 # populate map
@@ -48,7 +47,6 @@ for l in closest_locations:
     lat = closest_locations[l]["lat"]
     long = closest_locations[l]["long"]
 
-    print(name)
     folium.Marker([lat,long], icon=folium.Icon(icon="info-sign")).add_to(map_uk)
     folium.Marker([lat,long], icon=DivIcon(icon_size=(250,30),icon_anchor=(0,0), html=f'<div style="font-size: 20pt" <div>{name}</div>',)).add_to(map_uk)
 
